c9611e7cdb08c6db9451975a7763d7c5adfcaa9c
anandababu-annadurai <58395840+anandababu-annadurai@users.noreply.github.com>
2025-08-28 17:45:56 -0700

    Update ruby1.sh
    20.04

--- ruby1.sh
+++ ruby1.sh
@@ -1,5 +1,22 @@
 sudo apt-get update -y
-sudo apt-get install -y ruby3.0 ruby3.0-dev build-essential zlib1g-dev libssl-dev libreadline-dev
-sudo gem install bundler --no-document
+sudo apt-get install -y git build-essential libssl-dev libreadline-dev zlib1g-dev
+
+# Install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Add ruby-build plugin
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Install Ruby 3.1.6
+rbenv install 3.1.6
+rbenv global 3.1.6
+
+# Verify
 ruby -v
+
+# Install Bundler
+gem install bundler --no-document
 bundle -v
