4d53afbf85013df1c8d3c636a1bd8cbfe4737f64
anandababu-annadurai <58395840+anandababu-annadurai@users.noreply.github.com>
2025-08-28 18:12:09 -0700

    Update ruby1.sh
    ruby

--- ruby1.sh
+++ ruby1.sh
@@ -1,18 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "===== Installing Ruby 3.1.6 via rbenv ====="
+
+# Dependencies
 sudo apt-get update -y
 sudo apt-get install -y git build-essential libssl-dev libreadline-dev zlib1g-dev
 
-# Install rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+# rbenv setup
+if [ ! -d "$HOME/.rbenv" ]; then
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
 
-# Add ruby-build plugin
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# ruby-build plugin
+if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
 # Install Ruby 3.1.6
-rbenv install 3.1.6
-rbenv global 3.1.6
+~/.rbenv/bin/rbenv install -s 3.1.6
+~/.rbenv/bin/rbenv global 3.1.6
+
+# Refresh shell
+export PATH="$HOME/.rbenv/shims:$PATH"
 
 # Verify
 ruby -v
@@ -20,3 +34,5 @@ ruby -v
 # Install Bundler
 gem install bundler --no-document
 bundle -v
+
+echo "===== Ruby 3.1.6 + Bundler installed successfully ====="
